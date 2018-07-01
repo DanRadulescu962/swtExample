7a0a1fb95d03417a85361c00dedf7cbb487003f1
DanRadulescu962 <dan.radulescu@stud.acs.upb.ro>
2018-07-01 12:29:27 +0300

    Added swt jar

--- src/swtExample/SimpleShell.java
+++ src/swtExample/SimpleShell.java
@@ -1,9 +1,20 @@
 package swtExample;
 
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
 public class SimpleShell {
 
 	public static void main(String[] args) {
 		
+		Display display = new Display();
+		
+		Shell shell = new Shell(display);
+		
+		GridLayout layout = new GridLayout();
+		
+		shell.setLayout(layout);
 	}
 
 }
